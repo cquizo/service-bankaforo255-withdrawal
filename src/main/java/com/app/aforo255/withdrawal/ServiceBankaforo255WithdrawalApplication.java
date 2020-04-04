a0a9ac9b8fdce9047c527f5078c642c4112c79e2
Crispin Quizo <cqm.cris@gmail.com>
2020-04-04 15:24:48 -0400

    first commit

---
+++ src/main/java/com/app/aforo255/withdrawal/ServiceBankaforo255WithdrawalApplication.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.withdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankaforo255WithdrawalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankaforo255WithdrawalApplication.class, args);
+	}
+
+}
